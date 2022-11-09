1c2b1bf547dcae8f25e2be7f4c2aad0ad315eaf1
Mikita Hauruk <Mikita_Hauruk@epam.com>
2022-11-09 20:30:02 +0300

    Implemented API tests

---
+++ API/ApiUtilities.cs
@@ -0,0 +1,34 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GmailTA.API
+{
+    public class ApiUtilities
+    {
+        public static HttpWebResponse MakeRequest(HttpWebRequest request)
+        {
+            request.Method = "GET";
+            HttpWebResponse response = (HttpWebResponse)request.GetResponse();
+            return response;
+        }
+        public static string GetBody(HttpWebResponse response)
+        {
+            string responceBody = String.Empty;
+
+            using (Stream s = response.GetResponseStream())
+            {
+                using (StreamReader r = new StreamReader(s))
+                {
+                    responceBody = r.ReadToEnd();
+                }
+            }
+            return responceBody;
+        }
+        public static List<Root> DeserializeRootObject(string body) => JsonConvert.DeserializeObject<List<Root>>(body);
+    }
+}
---
+++ Tests/RestTest.cs
@@ -0,0 +1,38 @@
+﻿using GmailTA.API;
+using GmailTA.Test;
+using MongoDB.Bson.IO;
+using RestSharp;
+using System.Net;
+
+namespace GmailTA.Tests
+{
+    [TestFixture]
+    public class RestTest
+    {
+        static HttpWebRequest request = (HttpWebRequest)WebRequest.Create("https://jsonplaceholder.typicode.com/users");
+        readonly HttpWebResponse responce = ApiUtilities.MakeRequest(request);
+
+        [Test]
+        public void VerifyOkStatusCode()
+        {
+            Assert.That(responce.StatusCode, Is.EqualTo(HttpStatusCode.OK));
+        }
+        [Test]
+        public void VerifyHeader()
+        {
+            Assert.That(responce.ContentType, Is.EqualTo("application/json; charset=utf-8"));
+            Assert.IsNotEmpty(responce.Headers);
+
+        }
+        [Test]
+        public void VerfiyBody()
+        {
+            List<Root> rootObject = ApiUtilities.DeserializeRootObject(ApiUtilities.GetBody(responce));
+
+            Assert.That(rootObject.Count, Is.EqualTo(10));
+
+
+        }
+
+    }
+}
